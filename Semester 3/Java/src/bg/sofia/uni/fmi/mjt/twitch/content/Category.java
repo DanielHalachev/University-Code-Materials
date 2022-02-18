041775272ad77858b6dadcca719ef1b5753a23b8
Daniel Halachev <57269802+DanielHalachev@users.noreply.github.com>
2022-02-18 18:31:38 +0200

    Delete Semester 3/Java/src/bg/sofia/uni/fmi/mjt/twitch directory

--- Semester 3/Java/src/bg/sofia/uni/fmi/mjt/twitch/content/Category.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.twitch.content;
-
-public enum Category {
-    GAMES, IRL, MUSIC, ESPORTS
-}
--- Semester 3/Java/src/bg/sofia/uni/fmi/mjt/twitch/content/Metadata.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.twitch.content;
-
-import bg.sofia.uni.fmi.mjt.twitch.user.User;
-
-// Since Metadata content isn't supposed to change, Metadata is a record
-public record Metadata(String title, Category category, User streamer) {
-
-}
