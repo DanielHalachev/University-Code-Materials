2c90abb467cc6ef7bd941c3a9f2ff86aebaba10a
Daniel Halachev <57269802+DanielHalachev@users.noreply.github.com>
2022-02-19 22:48:41 +0200

    Add files via upload

---
+++ Semester 3/DIS-3 Sequence Solver/C#/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SequenceSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA1 = new System.Windows.Forms.TextBox();
+            this.textBoxA2 = new System.Windows.Forms.TextBox();
+            this.textBoxA3 = new System.Windows.Forms.TextBox();
+            this.textBoxA4 = new System.Windows.Forms.TextBox();
+            this.textBoxA5 = new System.Windows.Forms.TextBox();
+            this.textBoxA6 = new System.Windows.Forms.TextBox();
+            this.textBoxLambda = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.richTextBoxIndexes = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxA1
+            // 
+            this.textBoxA1.Location = new System.Drawing.Point(13, 13);
+            this.textBoxA1.Name = "textBoxA1";
+            this.textBoxA1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA1.TabIndex = 0;
+            // 
+            // textBoxA2
+            // 
+            this.textBoxA2.Location = new System.Drawing.Point(13, 39);
+            this.textBoxA2.Name = "textBoxA2";
+            this.textBoxA2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA2.TabIndex = 1;
+            // 
+            // textBoxA3
+            // 
+            this.textBoxA3.Location = new System.Drawing.Point(13, 65);
+            this.textBoxA3.Name = "textBoxA3";
+            this.textBoxA3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA3.TabIndex = 2;
+            // 
+            // textBoxA4
+            // 
+            this.textBoxA4.Location = new System.Drawing.Point(13, 91);
+            this.textBoxA4.Name = "textBoxA4";
+            this.textBoxA4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA4.TabIndex = 3;
+            // 
+            // textBoxA5
+            // 
+            this.textBoxA5.Location = new System.Drawing.Point(13, 117);
+            this.textBoxA5.Name = "textBoxA5";
+            this.textBoxA5.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA5.TabIndex = 4;
+            // 
+            // textBoxA6
+            // 
+            this.textBoxA6.Location = new System.Drawing.Point(13, 143);
+            this.textBoxA6.Name = "textBoxA6";
+            this.textBoxA6.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA6.TabIndex = 5;
+            // 
+            // textBoxLambda
+            // 
+            this.textBoxLambda.Location = new System.Drawing.Point(13, 169);
+            this.textBoxLambda.Name = "textBoxLambda";
+            this.textBoxLambda.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLambda.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(479, 270);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(108, 270);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 8;
+            this.buttonCalculate.Text = "button1";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(108, 317);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "button1";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(108, 361);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 10;
+            this.buttonClose.Text = "button1";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // richTextBoxIndexes
+            // 
+            this.richTextBoxIndexes.Location = new System.Drawing.Point(163, 30);
+            this.richTextBoxIndexes.Name = "richTextBoxIndexes";
+            this.richTextBoxIndexes.Size = new System.Drawing.Size(100, 96);
+            this.richTextBoxIndexes.TabIndex = 7;
+            this.richTextBoxIndexes.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxIndexes);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxLambda);
+            this.Controls.Add(this.textBoxA6);
+            this.Controls.Add(this.textBoxA5);
+            this.Controls.Add(this.textBoxA4);
+            this.Controls.Add(this.textBoxA3);
+            this.Controls.Add(this.textBoxA2);
+            this.Controls.Add(this.textBoxA1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA1;
+        private System.Windows.Forms.TextBox textBoxA2;
+        private System.Windows.Forms.TextBox textBoxA3;
+        private System.Windows.Forms.TextBox textBoxA4;
+        private System.Windows.Forms.TextBox textBoxA5;
+        private System.Windows.Forms.TextBox textBoxA6;
+        private System.Windows.Forms.TextBox textBoxLambda;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.RichTextBox richTextBoxIndexes;
+    }
+}
+
---
+++ Semester 3/DIS-3 Sequence Solver/C#/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SequenceSolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxLambda.Focus();
+            dataGridView1.RowHeadersVisible = false;
+        }
+
+        private double calculateDenominator(int b1, int b2, int b3, double previous)
+        {
+            return (b1 * previous * previous + b2 * previous + b3);
+        }
+        private double calculateMember(int a1, int a2, int a3, double denominator, double previous)
+        {
+            return (a1 * previous * previous + a2 * previous + a3) / denominator;
+        }
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int a1 = 0, a2 = 0, a3 = 0, b1 = 0, b2 = 0, b3 = 1;
+            double lambda = 0;
+            try
+            {
+                dataGridView1.Visible = true;
+                a1 = int.Parse(textBoxA1.Text);
+                a2 = int.Parse(textBoxA2.Text);
+                a3 = int.Parse(textBoxA3.Text);
+                b1 = int.Parse(textBoxA4.Text);
+                b2 = int.Parse(textBoxA5.Text);
+                b3 = int.Parse(textBoxA6.Text);
+                lambda = double.Parse(textBoxLambda.Text);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Нещо се обърка. Моля проверете входните данни!\n" + ex.Message);
+                return;
+            }
+            if (b1 * lambda * lambda + b2 * lambda + b3 == 0)
+            {
+                MessageBox.Show("Знаменателят не може да е нула! Въведете нова начална стойност!");
+                textBoxLambda.Clear();
+                textBoxLambda.Focus();
+                dataGridView1.Visible = false;
+                return;
+            }
+            List<int> indexes = new List<int>();
+            string[] indexStrings = richTextBoxIndexes.Text.Split(',');
+            try
+            {
+                if (indexStrings.Length == 0)
+                {
+                    throw new Exception("Не сте въвели индекси за изчисление!");
+                }
+                for (int i = 0; i < indexStrings.Length; i++)
+                {
+                    int temporary = int.Parse(indexStrings[i]);
+                    if (temporary < 1)
+                    {
+                        dataGridView1.Visible = false;
+                        throw new Exception("Невалиден отрицателен индекс за изчисление");
+                    }
+                    if (!indexes.Contains(temporary))
+                    {
+                        indexes.Add(temporary);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                richTextBoxIndexes.Clear();
+                richTextBoxIndexes.Focus();
+                dataGridView1.Visible = false;
+                return;
+            }
+            indexes.Sort();
+            Dictionary<int, double> results = new Dictionary<int, double>();
+            double temp = lambda;
+            int nextInIndexes = 0;
+            double denominator;
+            for (int i = 1; i <= indexes.Last(); i++)
+            {
+                denominator = calculateDenominator(b1, b2, b3, temp);
+                if (denominator == 0)
+                {
+                    MessageBox.Show("Член номер {0} не съществува, защото знаменателят е нула. Изчисляването прекратено.", i.ToString());
+                    break;
+                }
+                temp = calculateMember(a1, a2, a3, denominator, temp);
+                if (i == indexes[nextInIndexes])
+                {
+                    results.Add(i, temp);
+                    nextInIndexes++;
+                }
+            }
+            dataGridView1.Visible = true;
+            var indexValueArray = from row in results select new { Номер = row.Key, Стойност = row.Value };
+            dataGridView1.DataSource = indexValueArray.ToArray();
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            dataGridView1.Refresh();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxLambda.Clear();
+            richTextBoxIndexes.Clear();
+            dataGridView1.Visible = false;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
