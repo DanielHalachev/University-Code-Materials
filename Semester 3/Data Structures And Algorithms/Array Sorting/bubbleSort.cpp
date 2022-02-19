6b707cd6067505cf4b62fbc06a42710c5f248e4a
Daniel Halachev <57269802+DanielHalachev@users.noreply.github.com>
2022-02-19 21:06:26 +0200

    Delete Semester 3/Data Structures And Algorithms/Array Sorting directory

--- Semester 3/Data Structures And Algorithms/Array Sorting/bubbleSort.cpp
+++
@@ -1,9 +0,0 @@
-void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}
