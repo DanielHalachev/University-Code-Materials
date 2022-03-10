43d6217d28a164cde17409a150d0de89162035b6
Daniel Halachev <57269802+DanielHalachev@users.noreply.github.com>
2022-03-10 17:55:53 +0200

    Update task2.sql

--- Semester 4/Databases/Week3/task2.sql
+++ Semester 4/Databases/Week3/task2.sql
@@ -1,17 +1,17 @@
 --1
--- SELECT DISTINCT product.maker
--- FROM product
--- WHERE product.[type]='pc' AND product.model IN (SELECT pc.model FROM pc WHERE pc.speed>500);
+SELECT DISTINCT product.maker
+FROM product
+WHERE product.[type]='pc' AND product.model IN (SELECT pc.model FROM pc WHERE pc.speed>500);
 
 --2
--- SELECT printer.code, printer.model, printer.price
--- FROM printer
--- WHERE printer.price = (SELECT MAX(printer.price)FROM printer);
+SELECT printer.code, printer.model, printer.price
+FROM printer
+WHERE printer.price = (SELECT MAX(printer.price)FROM printer);
 
 --3
--- SELECT laptop.code, laptop.model, laptop.speed, laptop.ram, laptop.hd, laptop.price, laptop.screen
--- FROM laptop
--- WHERE laptop.speed < ALL(SELECT pc.speed FROM pc);
+SELECT laptop.code, laptop.model, laptop.speed, laptop.ram, laptop.hd, laptop.price, laptop.screen
+FROM laptop
+WHERE laptop.speed < ALL(SELECT pc.speed FROM pc);
 
 --4
 SELECT pr.model, r.price
@@ -19,20 +19,20 @@ FROM product pr, (SELECT pc.model AS model, pc.price AS price FROM pc UNION SELE
 WHERE pr.model=r.model AND r.price>=ALL(SELECT pc.price AS price FROM pc UNION SELECT laptop.price FROM laptop UNION SELECT printer.price FROM printer);
 
 -- 5
--- SELECT product.maker
--- FROM product
--- WHERE product.type = 'printer' AND product.model IN (SELECT printer.model FROM printer WHERE printer.price=(SELECT MIN(printer.price)FROM printer WHERE printer.color='y')AND printer.color='y')
+SELECT product.maker
+FROM product
+WHERE product.type = 'printer' AND product.model IN (SELECT printer.model FROM printer WHERE printer.price=(SELECT MIN(printer.price)FROM printer WHERE printer.color='y')AND printer.color='y')
 
 --6
--- SELECT DISTINCT product.maker
--- FROM product
--- WHERE product.[type]='pc' AND product.model IN 
--- (SELECT pc.model FROM PC WHERE pc.speed=
---     (SELECT MAX(pc.speed)
---     FROM pc
---     WHERE pc.model IN 
---         (SELECT pc.model FROM pc WHERE pc.ram=
---             (SELECT MIN(pc.ram) FROM pc)
---         )
---     )
--- )
+SELECT DISTINCT product.maker
+FROM product
+WHERE product.[type]='pc' AND product.model IN 
+(SELECT pc.model FROM PC WHERE pc.speed=
+    (SELECT MAX(pc.speed)
+    FROM pc
+    WHERE pc.model IN 
+        (SELECT pc.model FROM pc WHERE pc.ram=
+            (SELECT MIN(pc.ram) FROM pc)
+        )
+    )
+)
